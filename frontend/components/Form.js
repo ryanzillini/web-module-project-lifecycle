cb42bc1acbeee576f4a578d1be9fe776933b3c2b
Ryan Zillini <nyrtrading134@gmail.com>
2023-06-05 14:21:56 -0400

    completed functionality and moving components to seperate files

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default class Form extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+        <form id="todoForm" onSubmit={this.props.onTodoFormSubmit}>
+          <input
+            onChange={this.props.onTodoNameInputChange}
+            value={this.props.todoNameInput}
+            type="text"
+            placeholder="Type todo"
+          ></input>
+          <input type="submit"></input>
+        </form>
+        <button onClick={this.props.toggleDisplayCompleteds}>
+          {this.props.toggleDisplayCompleteds ? "Hide" : "Show"} Completed
+        </button>
+      </div>
+    );
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default class TodoList extends React.Component {
   render() {
-    return null
+    return (
+      <div id="todos">
+        <h2>Todos:</h2>
+        {this.props.todos.reduce((acc, td) => {
+          if (this.props.displayCompleteds || !td.completed)
+            return acc.concat(
+              <div onClick={this.props.toggleCompleted(td.id)} key={td.id}>
+                {td.name}
+                {td.completed ? "check" : ""}
+              </div>
+            );
+          return acc;
+        }, [])}
+      </div>
+    );
   }
 }
