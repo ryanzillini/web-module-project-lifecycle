ea116e14904a6e227dc98211f5360fb0e9201f2e
Ryan Zillini <nyrtrading134@gmail.com>
2023-06-05 12:30:32 -0400

    Made functions

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,32 +8,66 @@ export default class App extends React.Component {
     super();
     this.state = {
       todos: [],
+      error: "",
+      todoNameInput: "",
     };
   }
+
+  postNewTodo = () => {
+    axios
+      .post(URL, { name: this.state.todoNameInput })
+      .then((res) => {
+        this.fetchAllTodos();
+        this.resetForm();
+      })
+      .catch(this.setAxiosResponseError);
+  };
+
+  resetForm = () => {
+    this.setState({ ...this.state, todoNameInput: "" });
+  };
+  setAxiosResponseError = () => {
+    (err) => this.setState({ ...this.state, error: err.response.data.message });
+  };
 
   fetchAllTodos = () => {
     axios
       .get(URL)
       .then((res) => this.setState({ ...this.state, todos: res.data.data }))
-      .catch((err) => console.error(err));
+      .catch(this.setAxiosResponseError());
+  };
+
+  onTodoFormSubmit = (evt) => {
+    evt.preventDefault();
+    this.postNewTodo();
   };
 
   componentDidMount() {
     this.fetchAllTodos();
   }
 
+  onTodoNameInputChange = (evt) => {
+    const { value } = evt.target;
+    this.setState({ ...this.state, todoNameInput: value });
+  };
+
   render() {
     return (
       <div>
-        <div id="error">Error: No error here</div>
+        <div id="error">Error: {this.state.error}</div>
         <div id="todos">
           <h2>Todos:</h2>
           {this.state.todos.map((td) => {
             return <div key={td.id}>{td.name}</div>;
           })}
         </div>
-        <form id="todoForm">
-          <input type="text" placeholder="Type todo"></input>
+        <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
+          <input
+            onChange={this.onTodoNameInputChange}
+            value={this.state.todoNameInput}
+            type="text"
+            placeholder="Type todo"
+          ></input>
           <input type="submit"></input>
           <button>Clear Completed</button>
         </form>
